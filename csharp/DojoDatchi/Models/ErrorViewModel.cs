84f25df7cad303e853412470574ee0bd1accff71
makiroggers <makiroggers@gmail.com>
2017-10-23 06:02:39 -0700

    Added DojoDatchi files and upadted .gitignore.

---
+++ csharp/DojoDatchi/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DojoDatchi.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
