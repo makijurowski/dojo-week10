c9c1ad51cac4c05dd8417f2bc0614a47ed02b190
makiroggers <makiroggers@gmail.com>
2017-10-24 06:04:16 -0700

    Added player actions, images, and dojodachi reactions.

--- csharp/DojoDatchi/Controllers/HomeController.cs
+++ csharp/DojoDatchi/Controllers/HomeController.cs
@@ -17,21 +17,84 @@ namespace DojoDatchi.Controllers
         [Route("dojodachi")]
         public IActionResult Dojodachi()
         {
-            var myDachi = HttpContext.Session.GetObjectFromJson<DojodachiInfo>("myDachi");
+            DojodachiInfo myDachi = HttpContext.Session.GetObjectFromJson<DojodachiInfo>("myDachi");
+            ViewBag.Image = "/images/pusheen_normal.gif";
+            ViewBag.Status = "active";
             if (myDachi == null)
             {
                 myDachi = new DojodachiInfo();
                 HttpContext.Session.SetObjectAsJson("myDachi", new DojodachiInfo());
+                ViewBag.Reaction = "";
+                ViewBag.Message = "Welcome to Dojodachi! What should kitty do?";
             }
-
             ViewBag.Dojodachi = myDachi;
-            ViewBag.Status = "playing";
-            ViewBag.Reaction = "";
-            ViewBag.Message = "Welcome to Dojodachi! Ain't he a cutie?";
-
             return View();
-        } 
+        }
+
+        [HttpPost]
+        [Route("dojodachi/action")]
+        public IActionResult Action(string action)
+        {
+            DojodachiInfo myDachi = HttpContext.Session.GetObjectFromJson<DojodachiInfo>("myDachi");
+            // Default values
+            ViewBag.Status = "active";
+            ViewBag.Message = "You performed an action!";
+
+            switch(action)
+            {
+                case "Feed":
+                    ViewBag.Reaction = "Dojodachi likes food!";
+                    myDachi.Fullness += 10;
+                    myDachi.Meals--;
+                    break;
+
+                case "Play":
+                    ViewBag.Reaction = "Dojodachi likes playing!";
+                    myDachi.Happiness += 20;
+                    break;
+
+                case "Work":
+                    ViewBag.Reaction = "Dojodachi hates working!";
+                    myDachi.Happiness -= 10;
+                    break;
+
+                case "Sleep":
+                    ViewBag.Reaction = "Dojodachi likes sleeping!";
+                    myDachi.Energy += 20;
+                    break;
+
+                default:
+                    ViewBag.Reaction = "Dojodachi says you suck!";
+                    break;
+            }
+            if (myDachi.Energy >= 100 || myDachi.Fullness >= 100 || myDachi.Happiness >= 100)
+            {
+                ViewBag.Status = "gameover";
+                ViewBag.Reaction = "";
+                ViewBag.Message = "You won the game!";
+            }
+            else if (myDachi.Fullness <= 0 || myDachi.Happiness <= 0)
+            {
+                ViewBag.Status = "gameover";
+                ViewBag.Reaction = "";
+                ViewBag.Message = "You lost the game!";
+            }
+            HttpContext.Session.SetObjectAsJson("myDachi", myDachi);
+            ViewBag.Dojodachi = myDachi;
+            ViewBag.Image = "/images/pusheen_normal.gif";
+            return View("Dojodachi");
+        }
+
+        [HttpPost]
+        [Route("dojodachi/reset")]
+        public IActionResult Reset()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Dojodachi");
+        }
     }
+
+
 
     public static class SessionExtensions
     {
--- csharp/DojoDatchi/Models/Dojodachi.cs
+++ csharp/DojoDatchi/Models/Dojodachi.cs
@@ -16,3 +16,4 @@ namespace DojoDatchi
         }
     }
 }
+
