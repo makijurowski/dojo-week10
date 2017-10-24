3602a1f65b5bfb0193f9690a04a7235f88626030
makiroggers <makiroggers@gmail.com>
2017-10-23 21:30:47 -0700

    Fixed broken JS.

--- algorithms/BST.js
+++ algorithms/BST.js
@@ -1,20 +1,43 @@
 function BST() {
-    this.root = null;
+  var root = this.root;
 
-    function NewNode(val) {
-        this.value = val;
-        this.left = null;
-        this.right = null;
-    }
+  this.NewNode = function NewNode(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  };
 
-    function addNode(val) {
-        var node = new NewNode(val);
-        var pointer = this.root;
-        if (!this.root) {
-            this.root = node;
-        }
-        while (pointer) {
-            // Continue hereeeee
-        }
+  this.addNode = function addNode(val) {
+    var node = new this.NewNode(val);
+    var currentNode = root;
+    if (!root) {
+      root = node;
+      return;
     }
+    while (currentNode) {
+      if (node.val < currentNode.val) {
+        if (!currentNode.left) {
+          currentNode.left = node;
+          break;
+        } else {
+          currentNode = currentNode.left;
+        }
+      } else {
+        if (!currentNode.right) {
+          currentNode.right = node;
+          break;
+        } else {
+          currentNode = currentNode.right;
+        }
+      }
+    }
+    return;
+  };
 }
+
+var newTree = new BST();
+newTree.addNode(5);
+newTree.addNode(10);
+newTree.addNode(3);
+newTree.addNode(20);
+newTree.addNode(2);
