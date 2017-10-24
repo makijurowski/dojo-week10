79688546315f89e07d56b3c805578be9c0d23ee3
makiroggers <makiroggers@gmail.com>
2017-10-24 06:51:18 -0700

    Finished game, just need randomized images and messages for flair~

--- csharp/DojoDatchi/Controllers/HomeController.cs
+++ csharp/DojoDatchi/Controllers/HomeController.cs
@@ -11,98 +11,137 @@ namespace DojoDatchi.Controllers
 {
     public class HomeController : Controller
     {
-        // private static DojodachiInfo myDachi { get; set; }
-
+        // Dojodachi index
         [HttpGet]
         [Route("dojodachi")]
         public IActionResult Dojodachi()
         {
-            DojodachiInfo myDachi = HttpContext.Session.GetObjectFromJson<DojodachiInfo>("myDachi");
+            // Get Dojodachi information from session and initialize default values
+            DojodachiPet myDachi = HttpContext.Session.GetObjectFromJson<DojodachiPet>("myDachi");
             ViewBag.Image = "/images/pusheen_normal.gif";
             ViewBag.Status = "active";
+            // If no myDachi instance exists, create one and start the game
             if (myDachi == null)
             {
-                myDachi = new DojodachiInfo();
-                HttpContext.Session.SetObjectAsJson("myDachi", new DojodachiInfo());
+                myDachi = new DojodachiPet();
+                HttpContext.Session.SetObjectAsJson("myDachi", new DojodachiPet());
                 ViewBag.Reaction = "";
                 ViewBag.Message = "Welcome to Dojodachi! What should kitty do?";
             }
+            // Save myDachi to ViewBag session
             ViewBag.Dojodachi = myDachi;
             return View();
         }
 
+        // Player pressed interaction button
         [HttpPost]
         [Route("dojodachi/action")]
         public IActionResult Action(string action)
         {
-            DojodachiInfo myDachi = HttpContext.Session.GetObjectFromJson<DojodachiInfo>("myDachi");
+            // Fetch Dojodachi information from session
+            DojodachiPet myDachi = HttpContext.Session.GetObjectFromJson<DojodachiPet>("myDachi");
             // Default values
+            Random GameRandom = new Random();
             ViewBag.Status = "active";
-            ViewBag.Message = "You performed an action!";
-
+            // Logic for button interactions
             switch(action)
             {
                 case "Feed":
-                    ViewBag.Reaction = "Dojodachi likes food!";
-                    myDachi.Fullness += 10;
-                    myDachi.Meals--;
+                    if (myDachi.Meals > 0)
+                    {
+                        myDachi.Meals--;
+                        int RandomChance = GameRandom.Next(1, 5);
+                        if (RandomChance == 1)
+                        {
+                            ViewBag.Reaction = "Blegh! Dojodachi hates this food.";
+                        }
+                        else
+                        {
+                            myDachi.Fullness += GameRandom.Next(5, 11);
+                            ViewBag.Reaction = "Dojodachi likes to eat!";
+                        }
+                    }
+                    else
+                    {
+                        ViewBag.Reaction = "";
+                        ViewBag.Message = "You have no food left to feed Dojodachi! Put him to work!";
+                    }
                     break;
-
                 case "Play":
-                    ViewBag.Reaction = "Dojodachi likes playing!";
-                    myDachi.Happiness += 20;
+                    if (myDachi.Energy > 0)
+                    {
+                        myDachi.Energy -= 5;
+                        myDachi.Happiness += GameRandom.Next(5, 11);
+                        ViewBag.Reaction = "Dojodachi is happy to play with you!";
+                    }
+                    else
+                    {
+                        ViewBag.Reaction = "";
+                        ViewBag.Message = "Dojodachi has no energy to play! He needs to go to sleep first.";
+                    }
                     break;
-
                 case "Work":
-                    ViewBag.Reaction = "Dojodachi hates working!";
-                    myDachi.Happiness -= 10;
+                    if (myDachi.Energy > 0)
+                    {
+                        myDachi.Energy -= 5;
+                        ViewBag.Reaction = "Dojodachi hates working but loves to earn meals!";
+                        myDachi.Meals += GameRandom.Next(1, 4);
+                    }
+                    else
+                    {
+                        ViewBag.Reaction = "";
+                        ViewBag.Message = "Dojodachi has no energy to work! He needs to go to sleep first.";
+                    }
                     break;
-
                 case "Sleep":
-                    ViewBag.Reaction = "Dojodachi likes sleeping!";
-                    myDachi.Energy += 20;
+                    myDachi.Energy += 15;
+                    myDachi.Fullness -= 5;
+                    myDachi.Happiness -= 5;
+                    ViewBag.Reaction = "Dojodachi feels well-rested!";
                     break;
-
                 default:
-                    ViewBag.Reaction = "Dojodachi says you suck!";
+                    ViewBag.Message = "Dojodachi says you don't know what you're doing!";
                     break;
             }
-            if (myDachi.Energy >= 100 || myDachi.Fullness >= 100 || myDachi.Happiness >= 100)
+            // Player won the game
+            if (myDachi.Energy >= 100 && myDachi.Fullness >= 100 && myDachi.Happiness >= 100)
             {
                 ViewBag.Status = "gameover";
                 ViewBag.Reaction = "";
                 ViewBag.Message = "You won the game!";
             }
+            // Player lost the game
             else if (myDachi.Fullness <= 0 || myDachi.Happiness <= 0)
             {
                 ViewBag.Status = "gameover";
                 ViewBag.Reaction = "";
                 ViewBag.Message = "You lost the game!";
             }
+            // Save myDachi information to session
             HttpContext.Session.SetObjectAsJson("myDachi", myDachi);
             ViewBag.Dojodachi = myDachi;
             ViewBag.Image = "/images/pusheen_normal.gif";
             return View("Dojodachi");
         }
 
+        // Player pressed "Play Again?" button
         [HttpPost]
         [Route("dojodachi/reset")]
         public IActionResult Reset()
         {
+            // Reset the game by clearing session, then redirect to Dojodachi
             HttpContext.Session.Clear();
             return RedirectToAction("Dojodachi");
         }
     }
 
-
-
+    // Provided code to use Json data for session
     public static class SessionExtensions
     {
         public static void SetObjectAsJson(this ISession session, string key, object value)
         {
             session.SetString(key, JsonConvert.SerializeObject(value));
         }
-
         public static T GetObjectFromJson<T>(this ISession session, string key)
         {
             var value= session.GetString(key);
--- csharp/DojoDatchi/Models/Dojodachi.cs
+++ csharp/DojoDatchi/Models/Dojodachi.cs
@@ -1,13 +1,13 @@
 namespace DojoDatchi
 {
-    public class DojodachiInfo
+    public class DojodachiPet
     {
         public int Fullness { get; set; }
         public int Happiness { get; set; }
         public int Meals { get; set; }
         public int Energy { get; set; }
 
-        public DojodachiInfo()
+        public DojodachiPet()
         {
             Fullness = 20;
             Happiness = 20;
