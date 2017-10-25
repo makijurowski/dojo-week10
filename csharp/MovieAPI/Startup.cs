4c02df5ce15834597dcb642a960651d8d7ba93bb
makiroggers <makiroggers@gmail.com>
2017-10-24 22:28:34 -0700

    Added modified base project files.

---
+++ csharp/MovieAPI/Startup.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace MovieAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSession();
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder App, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+            if (env.IsDevelopment())
+            {
+                App.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                App.UseExceptionHandler("/Home/Error");
+            }
+            App.UseSession();
+            App.UseStaticFiles();
+            App.UseMvc();
+        }
+    }
+}
