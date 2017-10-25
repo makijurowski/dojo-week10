558032c5fea8d03dbc29b25197ebe1748f49aef7
makiroggers <makiroggers@gmail.com>
2017-10-25 11:51:35 -0700

    Project start base boilerplate changes complete.

--- csharp/FormSubmission/Controllers/FormController.cs
+++ csharp/FormSubmission/Controllers/FormController.cs
@@ -4,31 +4,25 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using FormSubmission;
 using FormSubmission.Models;
 
 namespace FormSubmission.Controllers
 {
-    public class HomeController : Controller
+    public class FormController : Controller
     {
+        [HttpGet]
+        [Route("")]
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
+        [HttpGet]
+        [Route("error")]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
