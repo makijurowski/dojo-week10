627b3aa47dab94d6194cf5e2a5f0eb5cafb2c8bc
makiroggers <makiroggers@gmail.com>
2017-10-29 15:05:52 -0700

    Updated .gitignore, added wwwroot files for selected projects.

---
+++ csharp/LoginRegistration/wwwroot/js/site.js
@@ -0,0 +1,22 @@
+﻿// $(function() {
+//   $("#login").validate({
+//     rules: {
+//       LogEmail: {
+//         required: true,
+//         email: true,
+//         minlength: 10
+//       },
+//       LogPassword: {
+//         required: true,
+//         minlength: 6
+//       }
+//       address: {
+//         required: true,
+//         rangelength: [10, 250]
+//       },
+//       message: {
+//         rangelength: [50, 1050]
+//       }
+//     }
+//   });
+// });
---
+++ csharp/TheWall/wwwroot/js/site.js
@@ -0,0 +1,22 @@
+﻿// $(function() {
+//   $("#Login").validate({
+//     rules: {
+//       LogEmail: {
+//         required: true,
+//         email: true,
+//         minlength: 10
+//       },
+//       LogPassword: {
+//         required: true,
+//         minlength: 6
+//       }
+//       address: {
+//         required: true,
+//         rangelength: [10, 250]
+//       },
+//       message: {
+//         rangelength: [50, 1050]
+//       }
+//     }
+//   });
+// });
